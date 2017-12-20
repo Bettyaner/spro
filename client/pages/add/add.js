bb4269c3e2e51bd8bd840e5800d4fc72e9221857
Betty-ugu <13656481523@163.com>
2017-12-20 17:31:31 +0800

    second

--- client/pages/add/add.js
+++ client/pages/add/add.js
@@ -1,9 +1,25 @@
+var app = getApp();
 Page({
   data:{
     text:"花费",
-    marks:"备注"
+    marks:"备注",
+    toast_isshow:true,
+    toast_text:""
   },
   onLoad:function(){
       
+  },
+  toast_hidden:function(){
+    this.setData({
+      toast_isshow:true
+    })
+  },
+  formSubmit:function(){
+    var pay = $("#pay").val();
+    console.log(pay);
+    this.setData({
+      toast_isshow:false,
+      toast_text:"提交成功"
+    })
   }
 })
